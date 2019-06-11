bfcbed6098d1444f87a337f483be0e19b8af9314
xupeng.guo <xupeng.guo@fengjr.com>
2019-06-11 17:48:40 +0800

    commit

--- src/test/java/test/Businesstest.java
+++ src/test/java/test/Businesstest.java
@@ -4,11 +4,18 @@ import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.BucketOrder;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Cardinality;
+import org.elasticsearch.search.aggregations.metrics.ValueCount;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
@@ -34,6 +41,8 @@ public class Businesstest extends ElasticSearchTest {
      * 4. 根据时间段统计该段时间内身份证号进件数量，对进件数量排序
      * 5. 根据姓名时间段及产品类型查询
      * 6. 根据渠道号分组计算个数 后排序
+     * 7. select count(Distinct card_no) from temp_asset_info where create_time> xxx and create_time < yyyy and product_type_code=GXD
+     * 8. select count(*) from temp_asset_info where create_time> xxx and create_time < yyyy and product_type_code=GXD
      */
 
 
@@ -125,6 +134,140 @@ public class Businesstest extends ElasticSearchTest {
             SearchHit[] hitsEntities = hits.getHits();// 如果未找到,length=0
             logger.info("testGetAssetInfosByRangeDate find data's size = {}.", hitsEntities.length);
             Stream.of(hitsEntities).forEach(x -> System.out.println("testGetAssetInfosByRangeDate :" + x.getSourceAsString()));
+        }
+    }
+
+    /**
+     * 根据时间段统计该段时间内身份证号进件数量，对进件数量排序
+     */
+    @Test
+    public void testGetAssetInfoByMutiCondition1() throws IOException {
+        SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+
+        RangeQueryBuilder timeRange = QueryBuilders.rangeQuery("create_time").from("2019-01-10 00:00:00").to("2019-01-11 00:00:00");
+        timeRange.includeLower(false);
+        timeRange.includeUpper(false);
+
+        TermsAggregationBuilder field = AggregationBuilders.terms("cardNumGroup").field("card_no");
+        AggregationBuilders.dateRange("dateRange").addRange("2019-01-10 00:00:00", "2019-01-11 00:00:00");
+        field.order(BucketOrder.count(false));
+        field.size(1100);
+        SearchSourceBuilder ssb = new SearchSourceBuilder();
+        ssb.size(0);
+        ssb.query(timeRange);
+        ssb.aggregation(field);
+        searchRequest.source(ssb);
+        // 4. 查询，可能抛出异常
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        if (response.status() == RestStatus.OK) {
+            Terms terms = response.getAggregations().get("cardNumGroup");
+            terms.getBuckets().forEach((bucket) -> {
+                logger.info("testGetAssetInfoByMutiCondition results = {}.", bucket.getKey() + ":" + bucket.getDocCount());
+            });
+        }
+    }
+
+    /**
+     * 根据姓名时间段及产品类型查询
+     */
+    @Test
+    public void testGetAssetInfoByMutiCondition2() throws IOException {
+        SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+
+        RangeQueryBuilder timeRange = QueryBuilders.rangeQuery("create_time").from("2019-01-10 00:00:00").to("2019-01-11 00:00:00");
+        timeRange.includeLower(false);
+        timeRange.includeUpper(false);
+
+
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(QueryBuilders.matchQuery("user_name", "5a1v9yNo78ACMNgxAexyUg=="));
+        boolQueryBuilder.must(QueryBuilders.termQuery("channel_code", "BEIDOU"));
+        boolQueryBuilder.must(timeRange);
+        SearchSourceBuilder ssb = new SearchSourceBuilder();
+        ssb.query(boolQueryBuilder);
+        searchRequest.source(ssb);
+        // 4. 查询，可能抛出异常
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        if (response.status() == RestStatus.OK) {
+            SearchHits hits = response.getHits();
+            assertNotNull(hits);
+            SearchHit[] hitsEntities = hits.getHits();// 如果未找到,length=0
+            logger.info("testGetAssetInfoByMutiCondition2 find data's size = {}.", hitsEntities.length);
+            Stream.of(hitsEntities).forEach(x -> System.out.println("testGetAssetInfoByMutiCondition2 :" + x.getSourceAsString()));
+        }
+    }
+
+    /**
+     * 根据渠道号和产品类型分组计算个数 后排序
+     */
+    @Test
+    public void testGetAssetInfoByChannelCodeProductTypeCode() throws IOException {
+        SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+
+
+        TermsAggregationBuilder field = AggregationBuilders.terms("channelCodeCount").field("channel_code");
+        field.order(BucketOrder.count(false));
+        field.size(1100);
+
+        TermsAggregationBuilder field2 = AggregationBuilders.terms("productTypeCode").field("product_type_code");
+        field.order(BucketOrder.count(false));
+        field.size(1100);
+
+        SearchSourceBuilder ssb = new SearchSourceBuilder();
+        ssb.size(0);
+        ssb.aggregation(field);
+        searchRequest.source(ssb);
+        // 4. 查询，可能抛出异常
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        if (response.status() == RestStatus.OK) {
+            Terms terms = response.getAggregations().get("channelCodeCount");
+            terms.getBuckets().forEach((bucket) -> {
+                logger.info("testGetAssetInfoByMutiCondition results = {}.", bucket.getKey() + ":" + bucket.getDocCount());
+            });
+        }
+    }
+
+    /**
+     * 类似于select count(Distinct card_no) from temp_asset_info where create_time> xxx and create_time < yyyy and product_type_code=GXD
+     */
+    @Test
+    public void testGetAssetInfoCount1() throws IOException {
+        SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+        SearchSourceBuilder ssb = new SearchSourceBuilder();
+
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(QueryBuilders.rangeQuery("create_time").from("2019-01-10 00:00:00").to("2019-01-15 00:00:00"));
+        boolQueryBuilder.filter(QueryBuilders.termQuery("product_type_code", "GXD"));
+        ssb.query(boolQueryBuilder).aggregation(AggregationBuilders.cardinality("realCount").field("card_no"));
+        ssb.size(0);
+        searchRequest.source(ssb);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        if (response.status() == RestStatus.OK) {
+            Cardinality realCount = response.getAggregations().get("realCount");
+            logger.info("testGetAssetInfoCount1,count = {}", realCount.getValue());
+        }
+    }
+
+    /**
+     * 类似于select count(*) from temp_asset_info where create_time> xxx and create_time < yyyy and product_type_code=GXD
+     */
+    @Test
+    public void testGetAssetInfoCount2() throws IOException {
+        SearchRequest searchRequest = new SearchRequest(INDEX_NAME);
+        SearchSourceBuilder ssb = new SearchSourceBuilder();
+
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(QueryBuilders.rangeQuery("create_time").from("2019-01-10 00:00:00").to("2019-01-15 00:00:00"));
+        boolQueryBuilder.filter(QueryBuilders.termQuery("product_type_code", "GXD"));
+        ssb.query(boolQueryBuilder).aggregation(AggregationBuilders.count("realCount").field("card_no"));
+        ssb.size(0);
+        searchRequest.source(ssb);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        if (response.status() == RestStatus.OK) {
+            ValueCount realCount = response.getAggregations().get("realCount");
+            logger.info("testGetAssetInfoByQueryConditionAndCountDistinct,count = {}", realCount.getValue());
         }
     }
 }
--- src/test/java/test/SearchRequestTest.java
+++ src/test/java/test/SearchRequestTest.java
@@ -6,9 +6,22 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.IndicesOptions;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.core.CountRequest;
+import org.elasticsearch.client.core.CountResponse;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.BucketOrder;
+import org.elasticsearch.search.aggregations.PipelineAggregatorBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.*;
+import org.elasticsearch.search.aggregations.pipeline.StatsBucket;
+import org.elasticsearch.search.aggregations.pipeline.StatsBucketPipelineAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
@@ -92,7 +105,7 @@ public class SearchRequestTest extends ElasticSearchTest {
 //        });
     }
 
-    @Test
+    //    @Test
     public void testMutiSearchRequest() throws IOException {
         MultiSearchRequest request = new MultiSearchRequest();
         SearchRequest firstSearchRequest = new SearchRequest("asset_info");
@@ -113,6 +126,90 @@ public class SearchRequestTest extends ElasticSearchTest {
                 System.out.println(hit.getSourceAsString() + "--" + hit.getIndex());
             }
 
+        }
+    }
+
+    @Test
+    public void testCountRequest() throws IOException {
+        CountRequest countRequest = new CountRequest("temp_asset_info");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.termQuery("card_no", "65dk3bdkPg+r2qmmd7LdwIMdzPpOkYZqTv3l1Tv8/n4="));
+        countRequest.source(searchSourceBuilder);
+        CountResponse response = client.count(countRequest, RequestOptions.DEFAULT);
+        if (response.status() == RestStatus.OK) {
+            logger.info("count : {}", response.getCount());
+        }
+    }
+
+    @Test
+    public void testGroupCount() throws IOException {
+//        ValueCountAggregationBuilder field1 = AggregationBuilders.count("gradeAgg").field("card_no");
+//        SearchSourceBuilder searchSourceBuilder1= new SearchSourceBuilder();
+//        searchSourceBuilder1.aggregation(field1);
+//        CountRequest countRequest = new CountRequest("temp_asset_info");
+//        countRequest.source(searchSourceBuilder1);
+//        CountResponse r = client.count(countRequest, RequestOptions.DEFAULT);
+//        if (r.status() == RestStatus.OK) {
+//            logger.info("count : {}", r.getCount());
+//        }
+
+        /**
+         * 根据card_no 统计每个card_no 有多少数量
+         */
+        TermsAggregationBuilder field = AggregationBuilders.terms("gradeAgg").field("card_no");
+        field.size(10);// 结果数据]
+        /**
+         * BucketOrder.count(boolean asc)
+         按匹配文档格式升序/降序排序。
+         BucketOrder.key(boolean asc)
+         按key的升序或降序排序。
+         BucketOrder.aggregation
+         通过定义一个子聚合进行排序。
+         BucketOrder.compound(List< BucketOrder> orders)
+         创建一个桶排序策略，该策略根据多个条件对桶进行排序。
+         */
+        field.order(BucketOrder.count(false));
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.size(0);// 每条明细数据不返回
+        searchSourceBuilder.aggregation(field);
+
+        SearchRequest sr = new SearchRequest("temp_asset_info");
+        sr.source(searchSourceBuilder);
+
+        SearchResponse response = client.search(sr, RequestOptions.DEFAULT);
+        Terms aggregations = response.getAggregations().get("gradeAgg");
+        for (Terms.Bucket bucket : aggregations.getBuckets()) {
+            System.out.println(bucket.getKey() + ":" + bucket.getDocCount());
+        }
+    }
+
+
+    @Test
+    public void testGroupMaxMin() throws IOException {
+        // 统计
+        TermsAggregationBuilder tongji = AggregationBuilders.terms("gradeAgg").field("card_no");
+        tongji.size(10);// 结果数据
+        tongji.order(BucketOrder.count(false));
+        // 统计后的值
+
+        tongji.subAggregation(AggregationBuilders.max("maxId").field("id"));
+        tongji.subAggregation(AggregationBuilders.min("minId").field("id"));
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.size(0);// 每条明细数据不返回
+        searchSourceBuilder.aggregation(tongji);
+
+        SearchRequest sr = new SearchRequest("temp_asset_info");
+        sr.source(searchSourceBuilder);
+
+        SearchResponse response = client.search(sr, RequestOptions.DEFAULT);
+
+        Terms aggregations = response.getAggregations().get("gradeAgg");
+        for (Terms.Bucket bucket : aggregations.getBuckets()) {
+            Max maxId = bucket.getAggregations().get("maxId");
+            Min minId = bucket.getAggregations().get("minId");
+            System.out.println(bucket.getKey() + ":" + bucket.getDocCount() + ":" + maxId.getValue() + ":"+ minId.getValue());
         }
     }
 }
